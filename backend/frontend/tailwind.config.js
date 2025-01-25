f43bdeec927c941cb5f778362fe8c296edcd7d42
zaid64-dev3 <kunownuser56@gmail.com>
2025-01-25 16:12:57 +0530

    Update tailwind.config.js

--- backend/frontend/tailwind.config.js
+++ backend/frontend/tailwind.config.js
@@ -1,13 +1,14 @@
-/** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/index.html',
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust the path to your project structure
+    './public/index.html',        // Include HTML if you're using it
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
 };
